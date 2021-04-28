e635fab48e97498cb2df88e5361c576bcb8fdf9f
cmm188 <76857753+cmm188@users.noreply.github.com>
2021-04-28 16:34:48 +0800

    Add files via upload

---
+++ Task/youthzl.js
@@ -0,0 +1,51 @@
+const axios = require('axios');
+
+
+ 
+    function randomString() {
+      let len = 32;
+      let $chars = "abcdefhijkmnprstwxyz123456789";
+      let maxPos = $chars.length;
+      let pwd = "";
+      for (let i = 0; i < len; i++) {
+        pwd += $chars.charAt(Math.floor(Math.random() * maxPos));
+      }
+      return pwd;
+    }
+    
+    
+
+    var aa=function(){
+      
+        var j = randomString();
+    
+    const url = 'https://script.baertt.com/count2/callback?si='+j+'&referer=https%253A%252F%252Ffocus.youth.cn%252Farticle%252Fs%253Fsignature%253DRQ3qz2nVgKk9rep70KLvrYCXzoKnSkRBZ5r4yXmv0ZJLWANOM5%2526uid%253D51526616%2526phone_code%253Da2823679662e562c3bb1fade2b2f3d5b%2526scid%253D37822898%2526time%253D1619561264%2526app_version%253D2.0.2%2526sign%253Da54847ebda3141e1e7285d29693edca2&_=1619561307802&jsonpcallback=jsonp6';
+    
+    
+    
+     return axios.get(url, {
+    
+      headers: {'Referer' : 'https://focus.youth.cn/',
+    'Host' : 'script.baertt.com',
+    'User-Agent' : 'Mozilla/5.0 (iPad; CPU OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.5(0x18000522) NetType/WIFI Language/zh_CN'} //设置header信息
+    }).then((res) => { 
+      //then 成功
+      console.log(j);
+    }).catch((err) => {
+      //catch 失败
+      console.log("失败")
+    
+    });
+  }
+
+
+const sleep = function (ms){
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+const request = async function(){
+  for(let i=0;i<50;i++){
+    aa();
+    await sleep(2000);
+  }
+}
+request();
