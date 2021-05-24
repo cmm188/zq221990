54c31f7853c833a22c2752573c08f42fd9b07304
cmm188 <76857753+cmm188@users.noreply.github.com>
2021-05-24 13:19:15 +0800

    Update youthzl.js

--- Task/youthzl.js
+++ Task/youthzl.js
@@ -24,3 +24,40 @@ const axios = require('axios');
         var j = randomString();
  var p = new Date().getTime();
     const url= 'https://script.baertt.com/count2/callback?si='+j+'&referer=https%253A%252F%252Ffocus.youth.cn%252Farticle%252Fs%253Fsignature%253DeQVjADm2pM09d8g4X60d3Xto30krCAeXNJkalyGPYqnLbZRBXK%2526uid%253D52759849%2526phone_code%253Df366260adcd3a6e11e53babb137a325c%2526scid%253D38576574%2526time%253D '+q+'%2526app_version%253D2.0.2%2526sign%253D1aa0291dd0dd616ef175a4f2bc53fc2e%2526from%253Dsinglemessage%2526isappinstalled%253D0&_= '+p+'&jsonpcallback=jsonp5 ';
+    
+    return axios.get(url, {
+    
+      headers: {'Referer' : 'https://focus.youth.cn/',
+    'Host' : 'script.baertt.com',
+    'User-Agent' : 'Mozilla/5.0 (iPad; CPU OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.5(0x18000522) NetType/WIFI Language/zh_CN'} //设置header信息
+    }).then((res) => { 
+      //then 成功
+      console.log(j);
+       console.log(p);
+    }).catch((err) => {
+      //catch 失败
+      console.log("失败")
+      
+    
+    });
+  }
+
+
+
+
+const sleep = function (ms){
+  return new Promise(resolve => setTimeout(resolve, ms))
+  
+}
+const request = async function(){
+  for(let i=0;i<50;i++){
+    aa();
+    var num=Math.ceil(Math.random() * 25);
+    await sleep(num*500);
+  }
+}
+
+
+
+
+request();
