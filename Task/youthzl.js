263fa66e4a8c28966cb0771c06b04f2523c30cb7
cmm188 <76857753+cmm188@users.noreply.github.com>
2021-05-26 07:10:28 +0800

    Update youthzl.js

--- Task/youthzl.js
+++ Task/youthzl.js
@@ -23,7 +23,69 @@ const axios = require('axios');
       
         var j = randomString();
  var p = new Date().getTime();
-    const url= 'https://script.baertt.com/count2/callback?si='+j+'&referer=https%253A%252F%252Ffocus.youth.cn%252Farticle%252Fs%253Fsignature%253DWn8Pym36L9l0Yoz1y9RrbLH8lKXOfY3vOzY1xKXjQqgZBMVdDe%2526uid%253D52759849%2526phone_code%253Df366260adcd3a6e11e53babb137a325c%2526scid%253D38485728%2526time%253D '+q+'%2526app_version%253D2.0.2%2526sign%253Dfdad6a75c0cab5d776ac0f8cab8e55cd%2526from%253Dsinglemessage%2526isappinstalled%253D0&_= '+p+'&jsonpcallback=jsonp6 ';
+    const url= 'https://script.baertt.com/count2/callback?si='+j+'const axios = require('axios');
+
+
+ //随机si
+    function randomString() {
+      let len = 32;
+      let $chars = "abcdefhijkmnprstwxyz123456789";
+      let maxPos = $chars.length;
+      let pwd = "";
+      for (let i = 0; i < len; i++) {
+        pwd += $chars.charAt(Math.floor(Math.random() * maxPos));
+      }
+      return pwd;
+    }
+    
+    
+    
+    //获取分享时间字符串
+    var q=Date.parse(new Date())/1000;
+    
+//get 函数
+    var aa=function(){
+      
+        var j = randomString();
+ var p = new Date().getTime();
+    const url= 'https://script.baertt.com/count2/callback?si='+j+'&referer=https%253A%252F%252Ffocus.youth.cn%252Farticle%252Fs%253Fsignature%253DMGDKgpQNLZkJvEd4q2x8yOuewApGcNoqJrl7rny295VAlmPWzY%2526uid%253D47273215%2526phone_code%253Df366260adcd3a6e11e53babb137a325c%2526scid%253D38096083%2526time%253D'+q+'%2526app_version%253D1.8.0%2526sign%253Dfcc0e14cc88191582bbaba5523683e13&_='+p+'&jsonpcallback=jsonp6';
+    
+    return axios.get(url, {
+    
+      headers: {'Referer' : 'https://focus.youth.cn/',
+    'Host' : 'script.baertt.com',
+    'User-Agent' : 'Mozilla/5.0 (iPad; CPU OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.5(0x18000522) NetType/WIFI Language/zh_CN'} //设置header信息
+    }).then((res) => { 
+      //then 成功
+      console.log(j);
+       console.log(p);
+    }).catch((err) => {
+      //catch 失败
+      console.log("失败")
+      
+    
+    });
+  }
+
+
+
+
+const sleep = function (ms){
+  return new Promise(resolve => setTimeout(resolve, ms))
+  
+}
+const request = async function(){
+  for(let i=0;i<50;i++){
+    aa();
+    var num=Math.ceil(Math.random() * 25);
+    await sleep(num*500);
+  }
+}
+
+
+
+5
+request();
     
     return axios.get(url, {
     
