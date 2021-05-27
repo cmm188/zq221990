d65c2bf8812125a138ed20a387f35e0517fbe354
cmm188 <76857753+cmm188@users.noreply.github.com>
2021-05-28 07:42:57 +0800

    Update youthzl.js

--- Task/youthzl.js
+++ Task/youthzl.js
@@ -23,7 +23,7 @@ const axios = require('axios');
       
         var j = randomString();
  var p = new Date().getTime();
-    const url= 'https://script.baertt.com/count2/callback?si='+j+'&referer=https%253A%252F%252Ffocus.youth.cn%252Farticle%252Fs%253Fsignature%253DKAn0BpeXzg3WkQRaAYXVrRuzrG8BfXvMmBbawr9G5ZDV6ldJ2N%2526uid%253D52759849%2526phone_code%253Df366260adcd3a6e11e53babb137a325c%2526scid%253D38610533%2526time%253D '+q+'%2526app_version%253D2.0.2%2526sign%253D48d811ca4cab873db4fa1bc6b034d05c%2526from%253Dsinglemessage%2526isappinstalled%253D0&_= '+p+'&jsonpcallback=jsonp6 ';
+    const url= 'https://script.baertt.com/count2/callback?si='+j+'&referer=https%253A%252F%252Ffocus.youth.cn%252Farticle%252Fshare_eightnew%253Fsignature%253DG6vDYbegoP2Owr90x84KkVogAUro5y54jVBXQnmqNLEMRyzZ3l%2526scene_id%253Dhome_feed%2526share_id%253D52759849386536911622156409%2526time%253D'+q+'&_='+p+'&jsonpcallback=jsonp6';
     
     return axios.get(url, {
     
